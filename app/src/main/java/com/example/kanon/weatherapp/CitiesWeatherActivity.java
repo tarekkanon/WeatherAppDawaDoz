cd1163cad189c57fd96fc6f24d4c57d11f34a913
Tarek Kanon <tarek.kanon@gmail.com>
2018-11-16 15:09:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kanon/weatherapp/CitiesWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.example.kanon.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CitiesWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cities_weather);
+    }
+}
